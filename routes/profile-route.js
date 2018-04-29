b4977d3baa10243bf16a11316ddc66279341fb75
Tapudp <divyeshpar2496@gmail.com>
2018-04-29 16:08:59 +0530

    all working also can render the post on profile page

--- routes/profile-route.js
+++ routes/profile-route.js
@@ -19,23 +19,29 @@ const authCheck = (req, res, next) => {
 
 router.post('/',  (req, res) => {
     var postData = new Post(req.body);
-    postData.save().then(result => {
-        console.log(result);
-        console.log(req.body);
-        res.redirect('/');
-    }).catch(err => {
-        res.status(404).send('Unable to save data to db');
-    });
+    if(postData.length){
+
+        postData.save().then(result => {
+            console.log(result);
+            console.log(req.body);
+            res.redirect('/');
+        }).catch(err => {
+            res.status(404).send('Unable to save data to db');
+        });
+    } else {
+        res.redirect('/profile');
+    }
     //res.send('you posted the the new blog with title ' + req.title + 'and content' +req.content) ;
     
 });
 
 router.get('/', authCheck, (req, res) => {
     //res.send('hi I\'m divyesh');
-    var post = Post.find((err, post) => {
+    Post.find({}, (err, posts) => {
         if(err) { console.log(err); }
-        else if (post.length){
-            console.log('Found: ', post);
+        else if (posts.length){
+            console.log('Found: ', posts);
+            res.render('profile', {user: req.user, posts: posts});
         }
         else {
             console.log('No document(s) found with defined "find" criteria!');
@@ -43,7 +49,6 @@ router.get('/', authCheck, (req, res) => {
     });
     
 
-    res.render('profile', {user: req.user, post: post});
     //res.render('profile', { user: req.user });
 });
 
